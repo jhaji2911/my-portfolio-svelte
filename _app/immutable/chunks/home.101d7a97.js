bb9905793650b0080006b460f2693d7368ee3c44
jhaji2911 <52101906+jhaji2911@users.noreply.github.com>
2024-11-02 17:55:48 +0000

    Deploying to gh-pages from @ jhaji2911/my-portfolio-svelte@a6dd882f87ff593adc50a5130ff2863717e5c827 🚀

--- _app/immutable/chunks/home.101d7a97.js
+++
@@ -1,5 +0,0 @@
-import{P as t}from"./types.62675a58.js";import{g as a}from"./skills.1e6be036.js";const e="Home",o="Nishant",i="Jha",l=`A seasoned full-stack developer with a passion for solving complex
-backend problems. With extensive experience in various JavaScript
-frameworks and Rust, I excel in creating scalable, efficient, and secure
-solutions. As a team lead, I collaborate with clients and partners to
-craft practical solutions that blend innovation with excellence`,c=[{platform:t.GitHub,link:"https://github.com/jhaji2911"},{platform:t.Linkedin,link:"https://www.linkedin.com/in/nishant-jha-29112000/"},{platform:t.X,link:"https://x.com/nonishantjha/"},{platform:t.StackOverflow,link:"https://stackoverflow.com/users/14529843/nishant-jha"},{platform:t.Email,link:"jhanishant2000@gmail.com"},{platform:t.Youtube,link:"https://www.youtube.com/channel/UCrEfVEpnuhHJWDC2_54jDNg"}],r=a("js","nodejs","reactjs","vue","sass","ts","rust","svelte");export{c as a,l as d,i as l,o as n,r as s,e as t};
