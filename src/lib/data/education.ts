63c31d5b91ee538b021f6291ab915210ebf6f5f5
nishantjha <jhanishant2000@gmail.com>
2024-11-02 23:33:43 +0530

    chore: added projects

--- src/lib/data/education.ts
+++ src/lib/data/education.ts
@@ -3,29 +3,17 @@ import type { Education } from '../types';
 
 export const items: Array<Education> = [
 	{
-		degree: 'Bachelor degree of Computer Science',
-		description: '',
-		location: 'Tunisia',
+		degree: 'Bachelor degree of Information Technology',
+		description: 'BS IT from Mumbai University',
+		location: 'India',
 		logo: Assets.Unknown,
 		name: '',
-		organization: 'ISTIC',
-		period: { from: new Date(2020, 0, 1), to: new Date(2022, 5, 1) },
+		organization: 'PCACS',
+		period: { from: new Date(2018, 0, 10), to: new Date(2021, 5, 1) },
 		shortDescription: '',
-		slug: 'dummy-education-item',
-		subjects: ['C', 'Algorithm', 'Algebra', 'Python', 'C++', 'Java', 'English']
+		slug: 'education-item',
+		subjects: ['C', 'Algorithm', 'Algebra', 'Python', 'C++', 'Java', 'English', 'JS', 'Embedded systems']
 	},
-	{
-		degree: 'PhD of Computer Science',
-		description: '',
-		location: 'USA',
-		logo: Assets.Unknown,
-		name: '',
-		organization: 'MIT',
-		period: { from: new Date(2023, 0, 1) },
-		shortDescription: '',
-		slug: 'dummy-education-item-2',
-		subjects: ['Assembly', 'Rust', 'Computer Architecture', 'Algorithms and Data structures']
-	}
 ];
 
 export const title = 'Education';
--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -6,63 +6,87 @@ export const items: Array<Project> = [
 	{
 		slug: 'ai-resume-analyzer',
 		color: '#5e95e3',
-		description: `Developed a transcription service leveraging Whisper.cpp for accurate speech-to-text conversion.
-Implemented Python TTS to convert text to natural-sounding speech, providing an end-to-end
-solution for audio processing.
-Created a cutting-edge proof of concept using local LLMs with LangChain in Node.js for custom
-tasks without added costs.`,
-		shortDescription: `Created a cutting-edge proof of concept using local LLMs with LangChain in Node.js for custom
-tasks without added costs.`,
+		description: `Created the app from scratch starting from orchestrating the architecture, frontend and backend, developed APIs to persist generated data. Designed Schema for backend structure. Created a cutting-edge proof of concept using local LLMs with LangChain in Node.js for custom tasks without added costs.`,
+		shortDescription: `Cutting-edge proof of concept using local LLMs with LangChain in Node.js for custom tasks.`,
 		links: [{ to: 'https://github.com/RiadhAdrani/slick-portfolio-svelte', label: 'GitHub' }],
-		logo: Assets.Unknown,
+		logo: Assets.NodeJs,
 		name: 'AI Resume Analyzer',
 		period: {
-			from: new Date()
+			from: new Date(2024, 0), // Start date in 2024
 		},
 		skills: getSkills('nodejs', 'ts', 'reactjs'),
 		type: 'Full stack app'
 	},
 	{
-		slug: 'slick-portfolio-svelte',
-		color: '#ff3e00',
-		description:
-			'A Vercel-like developer portfolio website template made with Typescript and SvelteKit.',
-		shortDescription:
-			'A Vercel-like developer portfolio website template made with Typescript and SvelteKit.',
-		links: [{ to: 'https://github.com/RiadhAdrani/slick-portfolio-svelte', label: 'GitHub' }],
-		logo: Assets.Svelte,
-		name: 'Slick Portfolio',
+		slug: 'arthfc-loan-app',
+		color: '#8e44ad',
+		description: `Created the entire app and website from scratch with React Native and ReactJS, added end-to-end encryption for security, implemented a reusable structure for ad hoc changes, and ensured type-safety in the API layer to handle backend changes seamlessly.`,
+		shortDescription: `Complete mobile and web app development with security and flexibility.`,
+		links: [],
+		logo: Assets.ReactNative,
+		name: 'ARTHFC Loan Mobile and Web App',
 		period: {
-			from: new Date()
+			from: new Date(2023, 0), // Start date in 2023
 		},
-		skills: getSkills('svelte', 'ts', 'tailwind', 'sass'),
-		type: 'Website Template',
-		screenshots: [
-			{
-				label: 'screen 1',
-				src: 'https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
-			},
-			{
-				label: '2',
-				src: 'https://images.unsplash.com/photo-1516116216624-53e697fedbea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
-			},
-			{
-				label: '3',
-				src: 'https://images.unsplash.com/photo-1537432376769-00f5c2f4c8d2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
-			},
-			{
-				label: '4',
-				src: 'https://images.unsplash.com/photo-1542903660-eedba2cda473?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
-			},
-			{
-				label: '5',
-				src: 'https://images.unsplash.com/photo-1619410283995-43d9134e7656?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
-			},
-			{
-				label: '6',
-				src: 'https://images.unsplash.com/photo-1585079542156-2755d9c8a094?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
-			}
-		]
+		skills: getSkills('react-native', 'reactjs', 'zustand', 'react-query'),
+		type: 'Mobile and Web App'
+	},
+	{
+		slug: 'bentley-learning-management-system',
+		color: '#3498db',
+		description: `Developed a robust learning management system for architectural software. Designed custom components, enhanced system resilience with multi-instance software, provided long-term support, and conducted R&D on AI/ML add-ons.`,
+		shortDescription: `Learning management system with resilient architecture and AI/ML add-ons.`,
+		links: [],
+		logo: Assets.Unknown,
+		name: 'Bentley Learning Management System',
+		period: {
+			from: new Date(2021, 0), // Start date in 2021
+			to: new Date(2023, 11), // End date in 2023
+		},
+		skills: getSkills('nextjs', 'react', 'react-native', 'nodejs'),
+		type: 'Full stack app'
+	},
+	{
+		slug: 'terril-bridgemarc-healthcare-frontend',
+		color: '#e67e22',
+		description: `Developed custom components for healthcare, ensured frontend security and compliance, mentored team members, and structured the solution for performance and security.`,
+		shortDescription: `Secure frontend solution with a focus on healthcare industry standards.`,
+		links: [],
+		logo: Assets.ReactJs,
+		name: 'Terril Bridgemarc Healthcare Frontend',
+		period: {
+			from: new Date(2023, 0), // Start date in 2023
+		},
+		skills: getSkills('nextjs', 'react'),
+		type: 'Frontend'
+	},
+	{
+		slug: 'txtsmarter-secure-app',
+		color: '#9b59b6',
+		description: `Developed an end-to-end encrypted application with transaction safety, resolved critical bugs, collaborated across domains, and built highly scalable frontend and backend microservices.`,
+		shortDescription: `End-to-end encrypted, transaction-safe app with a scalable architecture.`,
+		links: [],
+		logo: Assets.VueJs,
+		name: 'txtSmarter Secure Application',
+		period: {
+			from: new Date(2023, 0), // Start date in 2023
+		},
+		skills: getSkills('vue', 'nodejs'),
+		type: 'Full stack app'
+	},
+	{
+		slug: 'logitech-data-interface',
+		color: '#2ecc71',
+		description: `Translated complex business requirements, designed interfaces for large data volumes, implemented caching, streamlined processes with stakeholders, and revamped UI and backend for usability.`,
+		shortDescription: `Advanced data processing interface with caching and enhanced usability.`,
+		links: [],
+		logo: Assets.VueJs,
+		name: 'Logitech Data Interface',
+		period: {
+			from: new Date(2023, 0), // Start date in 2023
+		},
+		skills: getSkills('vue', 'moleculer', 'nodejs'),
+		type: 'Full stack app'
 	}
 ];
 
